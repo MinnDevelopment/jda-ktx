f75b3347954f13369eb44010196614e66dac7185
Florian Spieß <business@minnced.club>
2020-04-30 15:48:16 +0200

    Allow using await<Event> without coroutine manager

--- src/main/kotlin/dev/minn/jda/ktx/coroutines.kt
+++ src/main/kotlin/dev/minn/jda/ktx/coroutines.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.suspendCancellableCoroutine
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.GenericEvent
+import net.dv8tion.jda.api.hooks.EventListener
+import net.dv8tion.jda.api.hooks.SubscribeEvent
 import net.dv8tion.jda.api.requests.RestAction
 import net.dv8tion.jda.api.requests.restaction.pagination.PaginationAction
 import net.dv8tion.jda.api.utils.concurrent.Task
@@ -56,8 +58,9 @@ suspend fun <T> Task<T>.await() = suspendCancellableCoroutine<T> {
 }
 
 suspend inline fun <reified T : GenericEvent> JDA.await(crossinline filter: (T) -> Boolean = { true }) = suspendCancellableCoroutine<T> {
-    val listener = object : CoroutineEventListener {
-        override suspend fun onEvent(event: GenericEvent) {
+    val listener = object : EventListener {
+        @SubscribeEvent
+        override fun onEvent(event: GenericEvent) {
             if (event is T && filter(event)) {
                 removeEventListener(this)
                 it.resume(event)
--- src/main/kotlin/dev/minn/jda/ktx/events.kt
+++ src/main/kotlin/dev/minn/jda/ktx/events.kt
@@ -3,6 +3,9 @@ package dev.minn.jda.ktx
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.GenericEvent
 
+/**
+ * Requires [CoroutineEventManager] to be used!
+ */
 inline fun <reified T : GenericEvent> JDA.listener(crossinline consumer: suspend (T) -> Unit): CoroutineEventListener {
     return object : CoroutineEventListener {
         override suspend fun onEvent(event: GenericEvent) {
