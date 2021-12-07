3fe68c2288033b2359ba204fc7391c5223d0d675
Florian Spieß <business@minnced.club>
2021-12-07 22:26:39 +0100

    Make PaginationAction#produce use stable api

--- src/main/kotlin/dev/minn/jda/ktx/PaginationAction.kt
+++ src/main/kotlin/dev/minn/jda/ktx/PaginationAction.kt
@@ -16,15 +16,16 @@
 
 package dev.minn.jda.ktx
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.requests.restaction.pagination.PaginationAction
 import java.util.*
-import java.util.concurrent.CancellationException
 
 /**
  * Converts this PaginationAction to a [Flow]
@@ -50,28 +51,29 @@ fun <T, M: PaginationAction<T, M>> M.asFlow(): Flow<T> = flow {
 }
 
 /**
- * Converts this PaginationAction to a [ReceiveChannel]
+ * Converts this PaginationAction to a [ReceiveChannel].
+ *
+ * Remember to close the channel, or close the coroutine scope, once you don't need it anymore.
  *
  * @return [ReceiveChannel] instance
  */
-@ExperimentalCoroutinesApi
 suspend fun <T, M: PaginationAction<T, M>> M.produce(): ReceiveChannel<T> = coroutineScope {
-    produce {
-        cache(false)
-        val queue = LinkedList<T>()
-        try {
-            while (!isClosedForSend) {
-                if (queue.isEmpty())
-                    queue.addAll(await())
-                if (queue.isEmpty()) {
-                    close()
-                    break
-                }
-
-                while (!isClosedForSend && queue.isNotEmpty()) {
+    val channel = Channel<T>(1) // we don't need a buffer
+    cache(false)
+    launch {
+        channel.runCatching {
+            val queue = LinkedList<T>(await())
+            while (queue.isNotEmpty()) {
+                while (queue.isNotEmpty()) {
                     send(queue.poll())
                 }
+                queue.addAll(await())
             }
-        } catch (ignored: CancellationException) {}
+        }.onFailure {
+            if (it !is ClosedSendChannelException && it !is CancellationException)
+                throw it
+        }
     }
+
+    channel
 }
