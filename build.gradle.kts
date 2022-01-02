7d712aabb55c3770306357e6bbce848dbfc09542
Florian Spieß <business@minnced.club>
2022-01-02 17:50:35 +0100

    Use latest commit from interaction rework

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.dv8tion:JDA:5.0.0-alpha.2")
+    //compileOnly("net.dv8tion:JDA:5.0.0-alpha.2")
+    compileOnly("com.github.dv8fromtheworld:jda:78e4022e")
     api(kotlin("stdlib-jdk8"))
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
