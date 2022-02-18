b1c58f97fa4d43e28fafda23183ee75f5c3bb082
Florian Spieß <business@minnced.club>
2022-02-18 17:33:18 +0100

    Make JDA.scope return default

--- src/main/kotlin/dev/minn/jda/ktx/inject.kt
+++ src/main/kotlin/dev/minn/jda/ktx/inject.kt
@@ -17,7 +17,6 @@
 package dev.minn.jda.ktx
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
@@ -36,14 +35,12 @@ fun DefaultShardManagerBuilder.injectKTX(timeout: Duration = Duration.INFINITE) 
 
 /**
  * The coroutine scope used by the underlying [CoroutineEventManager].
- * If this instance does not use the coroutine event manager, this returns [GlobalScope] instead.
+ * If this instance does not use the coroutine event manager, this returns the default scope from [getDefaultScope].
  */
-val JDA.scope: CoroutineScope get() = (eventManager as? CoroutineEventManager) ?: GlobalScope
+val JDA.scope: CoroutineScope get() = (eventManager as? CoroutineEventManager) ?: getDefaultScope()
 
 /**
  * The coroutine scope used by the underlying [CoroutineEventManager].
- * If this instance does not use the coroutine event manager, this returns [GlobalScope] instead.
- *
- * @throws[NoSuchElementException] If no shards are currently available
+ * If this instance does not use the coroutine event manager, this returns the default scope from [getDefaultScope].
  */
-val ShardManager.scope: CoroutineScope get() = (shardCache.first().eventManager as? CoroutineEventManager) ?: GlobalScope
+val ShardManager.scope: CoroutineScope get() = (shardCache.firstOrNull()?.eventManager as? CoroutineEventManager) ?: getDefaultScope()
