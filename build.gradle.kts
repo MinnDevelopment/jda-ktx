5e0a0f2eed57b34cb9b53be5c3d8cbd059d67b8e
Florian Spieß <business@minnced.club>
2022-06-28 18:31:47 +0200

    Add perms v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "dev.minn"
-version = "0.9.1-alpha.11"
+version = "0.9.1-alpha.13"
 
 configure<JavaPluginExtension> {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -47,7 +47,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.dv8tion:JDA:5.0.0-alpha.11")
+    compileOnly("net.dv8tion:JDA:5.0.0-alpha.13")
     compileOnly("ch.qos.logback:logback-classic:1.2.10")
     compileOnly("club.minnced:discord-webhooks:0.7.5")
     api(kotlin("stdlib-jdk8"))
